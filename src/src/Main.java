8f9dc4a1a4cd391e0c8a059158189839ff30f8f1
Francescabattistini <166622198+Francescabattistini@users.noreply.github.com>
2024-09-27 14:08:22 +0200

    inizio for

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,5 +1,11 @@
+import entities.ElementoMultimediale;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        Scanner user = new Scanner(System.in);
+        ElementoMultimediale[] playlist = new ElementoMultimediale[5];
+        for(int i = 0; i < 5; i++){
     }
 }
