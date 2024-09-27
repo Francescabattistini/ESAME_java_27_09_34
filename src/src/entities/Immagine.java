b4124eb7c6b6c8e2a4393f3b939f6176545698b4
Francescabattistini <166622198+Francescabattistini@users.noreply.github.com>
2024-09-27 13:50:40 +0200

    img
    ripresa perchè ho fatto prima video

--- src/src/entities/Immagine.java
+++ src/src/entities/Immagine.java
@@ -3,4 +3,23 @@ package entities;
 public class Immagine extends ElementoMultimediale implements Static, Visibile{
 
 
+    @Override
+    public void play() {
+        
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void abbassaLuminosita() {
+
+    }
+
+    @Override
+    public void alzaLuminosita() {
+
+    }
 }
