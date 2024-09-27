25bf597486bfc5c1e8aa7b3e5c7211fa51cb3982
Francescabattistini <166622198+Francescabattistini@users.noreply.github.com>
2024-09-27 12:29:07 +0200

    inizio Audio

---
+++ src/src/entities/Audio.java
@@ -0,0 +1,22 @@
+package entities;
+
+public class Audio extends ElementoMultimediale implements Riproducibile {
+
+
+
+
+    @Override
+    public void play() {
+
+    }
+
+    @Override
+    public void alzaVolume() {
+
+    }
+
+    @Override
+    public Void abbassaVolume() {
+        return null;
+    }
+}
